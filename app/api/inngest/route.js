259bf922690d585fb13261c2aecfe5df11d90522
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2026-01-09 19:51:02 -0300

    .

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,4 +1,4 @@
-import { serve } from "inngest/next";
+import { serve } from "@inngest/next";
 import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/config/inngest";
 
 export const { GET, POST, PUT } = serve({
