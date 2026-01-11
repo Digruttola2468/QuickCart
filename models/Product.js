5afac2d804350b03b73921c2cf2fc5e825aec5f9
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2026-01-11 20:36:46 -0300

    .

---
+++ models/Product.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    userId: {type: String , required: true , ref: "user"},
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    offerPrice: { type: Number, default: 0 },
+    images: { type: Array, required: true },
+    date: { type: Date, default: Date.now, required: true },
+});
+
+const Product = mongoose.models.product || mongoose.model("product", productSchema);
+
+export default Product;
